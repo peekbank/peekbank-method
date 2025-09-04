34f2ffa13f2052e9dc1785ebd0a71d0076d9d376
vboyce <vero.boyce@gmail.com>
2025-09-04 11:03:36 -0700

    not working RT stuff

--- helper/common.R
+++ helper/common.R
@@ -1,4 +1,4 @@
-# source me for all analyses 
+# source me for all analyses
 
 library(here)
 library(tidyverse)
@@ -14,37 +14,41 @@ library(tictoc)
 
 # Seed for random number generation
 set.seed(42)
-knitr::opts_chunk$set(cache.extra = knitr::rand_seed, cache = TRUE, 
-                      message=FALSE, warning=FALSE, error=FALSE)
+knitr::opts_chunk$set(
+  cache.extra = knitr::rand_seed, cache = TRUE,
+  message = FALSE, warning = FALSE, error = FALSE
+)
 options(dplyr.summarise.inform = FALSE)
 
 # load data
-d_aoi <- readRDS(file = here("cached_intermediates","1_d_aoi.Rds"))
+# d_aoi <- readRDS(file = here("cached_intermediates","1_d_aoi.Rds"))
 
 # key ICC function
-get_icc <- function (x, column = "accuracy", object = "stimulus") {
+get_icc <- function(x, column = "accuracy", object = "stimulus") {
   if (object == "stimulus") {
-    iccs <- dim_icc(x, 
-                    model = "2A", 
-                    type = "consistency", 
-                    unit = "average",
-                    object = target_label, 
-                    rater = administration_id,
-                    trial = trial_id, 
-                    score = {{column}}, 
-                    bootstrap = 0)
+    iccs <- dim_icc(x,
+      model = "2A",
+      type = "consistency",
+      unit = "average",
+      object = target_label,
+      rater = administration_id,
+      trial = trial_id,
+      score = {{ column }},
+      bootstrap = 0
+    )
   } else {
-    iccs <- dim_icc(x, 
-                    model = "2A", 
-                    type = "consistency", 
-                    unit = "average",
-                    object = administration_id, 
-                    rater = target_label,
-                    trial = trial_id, 
-                    score = {{column}}, 
-                    bootstrap = 0)
+    iccs <- dim_icc(x,
+      model = "2A",
+      type = "consistency",
+      unit = "average",
+      object = administration_id,
+      rater = target_label,
+      trial = trial_id,
+      score = {{ column }},
+      bootstrap = 0
+    )
   }
-  
+
   return(iccs$Inter_ICC)
 }
 
@@ -54,14 +58,16 @@ options(dplyr.summarise.inform = FALSE)
 # .font <- "Source Sans Pro"
 # theme_set(theme_bw(base_size = 14, base_family = .font))
 theme_set(theme_bw(base_size = 10))
-theme_update(panel.grid = element_blank(),
-             strip.background = element_blank(),
-             legend.key = element_blank(),
-             panel.border = element_blank(),
-             axis.line = element_line(),
-             strip.text = element_text(face = "bold"))
+theme_update(
+  panel.grid = element_blank(),
+  strip.background = element_blank(),
+  legend.key = element_blank(),
+  panel.border = element_blank(),
+  axis.line = element_line(),
+  strip.text = element_text(face = "bold")
+)
 
 # helper function for scaling to the variance of a particular age group
-age_scale <- function (x, age, min = 16, max = 20) {
-  (x - mean(x, na.rm=TRUE)) / sd(x[age >= min & age <= max], na.rm=TRUE)
+age_scale <- function(x, age, min = 16, max = 20) {
+  (x - mean(x, na.rm = TRUE)) / sd(x[age >= min & age <= max], na.rm = TRUE)
 }
--- helper/rt_helper.R
+++ helper/rt_helper.R
@@ -1,7 +1,11 @@
 ## RT function tested in peekbank_rts.Rmd
 # takes rle_data dataframe (already rle'd)
-get_rt <- function(rle_data, SAMPLING_RATE = 40) {
+get_rt <- function(rle_data, SAMPLING_RATE = 40, t_0 = TRUE, window_length = 0, t_end = FALSE, window_mostly_region = FALSE) {
   # end if no data
+  print(rle_data)
+
+  print(rle_data$values)
+
   if (is.null(rle_data$values) | is.null(rle_data$lengths)) {
     return(tibble(
       rt = NA,
@@ -12,12 +16,68 @@ get_rt <- function(rle_data, SAMPLING_RATE = 40) {
   onset_aoi <- rle_data$values[1] # zero point AOI
 
   # end if missing for start
-  if (!(onset_aoi %in% c("target", "distractor"))) {
+  if (t_0 & !(onset_aoi %in% c("target", "distractor"))) {
     return(tibble(
       rt = NA,
-      shift_type = "other"
+      shift_type = "onset_not_td"
     ))
   }
+
+
+  first_departure_idx <- which(rle_data$values %in% c("target", "distractor"))[1]
+  if (is.na(first_departure_idx)) {
+    return(tibble(
+      rt = NA,
+      shift_type = "never_td"
+    ))
+  }
+
+  onset_aoi <- rle_data$values[first_departure_idx] # not technically onset, but we're going with it for consistency
+
+  other_aoi <- ifelse(onset_aoi == "distractor", "target", "distractor")
+
+  if (window_length > 0 & (t_end | window_mostly_region)) {
+    # TODO need to figure out from this data what is at end of window
+    elapsed <- cumsum(rle_data$lengths)
+    end_window_idx <- which(elapsed >= window_length)[1]
+    if (is.na(end_window_idx)) {
+      return(tibble(
+        rt = NA,
+        shift_type = "time_series_too_short"
+      ))
+    }
+    if (other_aoi %in% rle$values[1:end_window_idx]) {
+      return(tibble(
+        rt = NA,
+        shift_type = "both_td_in_window"
+      ))
+    }
+    end_window_value <- rle_data$values[end_window_idx]
+    if (t_end & end_window_value != onset_aoi) {
+      return(tibble(
+        rt = NA,
+        shift_type = "window_end_not_td"
+      ))
+    }
+    if (window_mostly_region) {
+      in_region_idx <- which(rle_data$values[1:end_window_idx - 1] == onset_aoi) # indices in time region to relevant region
+      if (length(in_region_idx) == 0) {
+        time_points <- 0
+      } else {
+        time_points <- sum(rle_data$lengths[in_region_idx])
+      }
+      if (end_window_value == onset_aoi) { # count the partial amount
+        time_points <- time_points + window_length - elapsed[end_window_idx - 1]
+      }
+      if (time_points < 3 / 4 * window_length) {
+        return(tibble(
+          rt = NA,
+          shift_type = "window_not_enough_td"
+        ))
+      }
+    }
+  }
+
 
   first_landing <- rle_data$values[rle_data$values != onset_aoi &
     rle_data$values %in% c("target", "distractor")][1]
@@ -76,7 +136,6 @@ get_rt <- function(rle_data, SAMPLING_RATE = 40) {
   shift_length <- landing_time_rt - shift_start_rt
 
   last_shift_length <- landing_time_rt - last_shift_rt
-
 
   return(tibble(
     rt = landing_time_rt, # define rt as landing time RT for now to avoid breaking things downstream
