5386b4b1fceee75049c02fbce3af5cb5695c1bb2
vboyce <vero.boyce@gmail.com>
2026-03-04 16:42:07 -0800

    add an option to eventually do by kid exclusions

--- scripts/4_acc_trial_cdi_boot.R
+++ scripts/4_acc_trial_cdi_boot.R
@@ -3,8 +3,10 @@ source("../helper/common.R")
 d_aoi <- readRDS("../cached_intermediates/0_d_aoi.rds")
 
 trial_flags <- d_aoi |>
-  group_by(dataset_name, trial_id, dataset_id, subject_id, administration_id,
-    target_label) |>
+  group_by(
+    dataset_name, trial_id, dataset_id, subject_id, administration_id,
+    target_label
+  ) |>
   summarise(
     total_target_prop = mean(correct, na.rm = TRUE),
     pre_looking = mean(correct[t_norm < 400], na.rm = TRUE),
@@ -15,16 +17,18 @@ d_aoi_age <- make_age_bins(d_aoi)
 
 cdi_data <- readRDS("../cached_intermediates/0_cdi_subjects.rds")
 
-acc_trial_cdi_summarize <- function(d, flags, t_start, t_end, exclude_less_than, look_both) {
+
+acc_trial_cdi_summarize <- function(d, flags, t_start, t_end, exclude_less_than, look_both, min_trials) {
   if (look_both == "ever") {
     flags <- filter(flags, total_target_prop > 0, total_target_prop < 1)
   } else if (look_both == "before") {
     flags <- filter(flags, pre_looking > 0, pre_looking < 1)
   }
 
+  }
   join_cols <- c("dataset_name", "trial_id", "dataset_id", "administration_id", "target_label")
 
-  d |>
+  valid_trial <- d |>
     semi_join(flags, by = join_cols) |>
     filter(t_norm > t_start, t_norm < t_end) |>
     group_by(across(all_of(c(
@@ -36,9 +40,14 @@ acc_trial_cdi_summarize <- function(d, flags, t_start, t_end, exclude_less_than,
       .groups = "drop"
     ) |>
     filter(prop_data >= exclude_less_than) |>
-    filter(!is.na(accuracy)) |>
+    filter(!is.na(accuracy)) |> 
     group_by(administration_id, dataset_name, across(any_of("age_bin"))) |>
-    summarize(mean_var = mean(accuracy, na.rm = T), .groups = "drop") |>
+    summarize(
+      mean_var = mean(accuracy, na.rm = T),
+      count = n(), .groups = "drop"
+    ) |>
+    filter(count < min_trials) |>
+    select(-count) |>
     filter(!is.na(mean_var))
 }
 
@@ -46,16 +55,23 @@ acc_params <- expand_grid(
   t_start = c(400),
   t_end = c(2000, 3000, 4000),
   exclude_less_than = c(0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1),
-  look_both = c("before", "ever", "no_need")
+  look_both = c("before", "ever", "no_need"),
+  min_trials = c(1) # no restrictions
+  # side_bias =c(1.5, 1, .95, .9), #1.5 is equal to none
+  # min_trials=c(1,2,3,4,5,8,10, 15)
 )
 
 accs_cdi_summarized <- acc_params |>
-  mutate(summary_data = pmap(list(t_start, t_end, exclude_less_than, look_both),
-    \(t_s, t_e, e, l) acc_trial_cdi_summarize(d_aoi, trial_flags, t_s, t_e, e, l)))
+  mutate(summary_data = pmap(
+    list(t_start, t_end, exclude_less_than, look_both, min_trials),
+    \(t_s, t_e, e, l, m) acc_trial_cdi_summarize(d_aoi, trial_flags, t_s, t_e, e, l, m)
+  ))
 
 accs_cdi_summarized_age <- acc_params |>
-  mutate(summary_data = pmap(list(t_start, t_end, exclude_less_than, look_both),
-    \(t_s, t_e, e, l) acc_trial_cdi_summarize(d_aoi_age, trial_flags, t_s, t_e, e, l)))
+  mutate(summary_data = pmap(
+    list(t_start, t_end, exclude_less_than, look_both, min_trials),
+    \(t_s, t_e, e, l, m) acc_trial_cdi_summarize(d_aoi_age, trial_flags, t_s, t_e, e, l, m)
+  ))
 
 rm(d_aoi, d_aoi_age, trial_flags)
 gc()
